171ea851dd63cf1fe8fba8e0f355c6bd83ba89fe
MaximoVeltri <maximoveltri@gmail.com>
2024-10-17 16:53:01 -0300

    renderizando usuarios

--- src/components/FetchApp.jsx
+++ src/components/FetchApp.jsx
@@ -2,30 +2,42 @@ import React from "react";
 import { useFetch } from "../hooks/useFetch";
 
 export const FetchApp = () => {
-  const { data, isLoading, errors } = useFetch();
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const { data, isLoading, error } = useFetch(url);
 
   return (
     <>
       <h1>Aplicacion de Hook: </h1>
       <hr />
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">id</th>
-            <td>name</td>
-            <td>email</td>
-            <td>website</td>
-          </tr>
-        </tbody>
-      </table>
+      {isLoading ? (
+        <h4>Cargando...</h4>
+      ) : error ? (
+        <h4>Ha ocurrido un error: {error}</h4>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data.map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <th scope="row">{user.id}</th>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.website}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useFetch = () => {
+export const useFetch = (url) => {
   const [state, setState] = useState({
     data: null,
     isLoading: true,
-    errors: null,
+    error: null,
   });
+
+  const { data, isLoading, error } = state;
+
+  const getFetch = async () => {
+    if (!url) return;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setState({
+        data,
+        isLoading: false,
+        error: error,
+      });
+    } catch (error) {
+      setState({
+        data: null,
+        error: error,
+        isLoading: false,
+      });
+    }
+
+    useState({
+      data,
+      isLoading: false,
+      error: null,
+    });
+  };
+
+  useEffect(() => {
+    getFetch();
+  }, []);
 
   return {
     data,
     isLoading,
-    errors,
+    error,
   };
 };
